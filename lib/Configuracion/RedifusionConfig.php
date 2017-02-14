004781f6144ac3bc1aef8eef776ba8c3b83e1a52
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-02-14 16:25:33 -0600

    Preparé la configuración.

---
+++ lib/Configuracion/RedifusionConfig.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * SiContamos Sitio Web - Redifusion Config
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package SiContamosSitioWeb
+ */
+
+namespace Configuracion;
+
+/**
+ * Clase RedifusionConfig
+ */
+class RedifusionConfig {
+
+    public $xml_encoding      = 'UTF-8';
+    public $sitio_titulo      = 'Sí Contamos';
+    public $sitio_url         = 'http://sicontamos.github.io'; // Sin diagonal al final
+    public $sitio_descripcion = 'Últimas publicaciones de Sí Contamos.';
+    public $lenguaje          = 'es-MX';
+    public $generator         = 'Plataforma de Conocimiento v3.0';
+    public $webmaster_email   = 'webmaster@trcimplan.mx (Webmaster)';
+    public $elementos_max     = 8;
+    public $archivo           = 'rss.xml';
+    public $usar_descripcion  = FALSE; // Si es falso, usará el contenido
+
+} // Clase RedifusionConfig
+
+?>
